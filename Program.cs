999a89e299cad201cfb8914d65daad9f5d2d41d8
exaul <137360873+exaul@users.noreply.github.com>
2024-01-06 11:55:01 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primera_clase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa tu nombre:");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Hola, " + nombre + "Bienvenida a tu primera clase");
+
+            Console.WriteLine("Presiona cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
